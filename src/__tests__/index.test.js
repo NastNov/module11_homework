f6bb06c16eb2570dd460d925ec9e45443dc8aea6
NastNov <111925797+NastNov@users.noreply.github.com>
2022-11-12 02:25:44 +0300

    Update index.test.js

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -4,6 +4,6 @@ test('countdown from 3 to 1', () => {
   expect(countDown(3)).toBe(3, 2, 1);
 });
 
-test('countdown from 3 to 1', () => {
+test('countdown from 0', () => {
   expect(countDown(0)).toBe(undefined);
 });
